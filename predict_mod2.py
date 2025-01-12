f361ff4861af2069be213352455f8a75a088085d
Mohamad Raslan <Refez.25@outlook.com>
2025-01-12 20:31:11 +0200

    Add files via upload

---
+++ predict_mod2.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+model = YOLO("best.pt") # use the trained model
+
+# source to 0 for local webcam 
+model.predict(source= "sample", show=True,
+            save = True, conf=0.2, line_thickness=2,
+            line_width=2,   
+            show_labels= True,
+            show_conf = False, 
+            ) 
